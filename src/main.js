6050b1437f7e07281a0a0d399cf2d43801fd4a68
wangweiwei <xd8012@163.com>
2018-09-20 01:06:40 +0800

    模板更新

--- src/main.js
+++ src/main.js
@@ -3,12 +3,31 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import store from './store/index'
+import store from './vuex/store'
+import Vuex from 'vuex'
+import Mock from './mock'
+Mock.bootstrap();
+
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css';
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.config.productionTip = false
 
 Vue.use(ElementUI)
-Vue.config.productionTip = false
+Vue.use(Vuex)
+
+router.beforeEach((to, from, next) => {
+  if (to.path == '/login') {
+    sessionStorage.removeItem('user');
+  }
+  let user = JSON.parse(sessionStorage.getItem('user'));
+  if (!user && to.path != '/login') {
+    next({ path: '/login' })
+  } else {
+    next()
+  }
+})
+
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,58 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Dashboard from '@/views/dashboard/Dashboard'
-
+import Home from '../views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/login',
+      component: (resolve)=> {
+        require(['../views/Login.vue'], resolve)
+      },
+      hidden:true
+    },
+    {
+      path: '/dashboard',
+      component: Home,
+      icon:'el-icon-edit',
+      children:[
+        {
+          path: '/dashboard',
+          component: function (resolve) {require(['../views/dashboard/index.vue'], resolve)},
+          name: '首页',
+        },
+      ]
+    },
+    {
       path: '/',
-      name: 'dashboard',
-      component: Dashboard,
+      component: Home,
+      icon:'el-icon-edit',
+      redirect:'/user',
+      children: [
+        {
+          path: '/user',
+          component: function (resolve) { require(['../views/example/user.vue'], resolve)},
+          name: '用户',
+          hidden:true
+        },
+      ]
+    },
+    {
+      path: '/',
+      component: Home,
+      name: '表格',
+      redirect:'/echarts',
+      icon:'el-icon-edit',
+      children: [
+        {
+          path: '/echarts',
+          component: function (resolve) { require(['../views/charts/echarts.vue'], resolve)},
+          name: 'echarts',
+          hidden:true
+        }
+      ]
     }
+
   ]
 })
